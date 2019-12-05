961cbcf7eff41312d66ace3a3d325d2104345225
yyavor <yyavorvoskyi@gmail.com>
2019-12-05 20:41:12 +0200

    Updated django admin to support custom model

--- app/core/models.py
+++ app/core/models.py
@@ -28,7 +28,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
-    username = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -15,7 +15,7 @@ class AdminSiteTests(TestCase):
         self.user = get_user_model().objects.create_user(
             email="usr1@mail.com",
             password="testpwd",
-            username="TestUserName"
+            name="TestUserName"
         )
 
     def test_user_is_listed(self):
@@ -24,5 +24,21 @@ class AdminSiteTests(TestCase):
         """
         url = reverse("admin:core_user_changelist")
         res = self.client.get(url)
-        self.assertContains(res, self.user.username)
+        self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page_open(self):
+        """
+        Test that change page is opened with correct status
+        """
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_user_create_page(self):
+        """
+        Test user create page is opened with correct status
+        """
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
